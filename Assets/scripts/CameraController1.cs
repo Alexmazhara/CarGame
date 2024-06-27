a63598b366338bc6403392d672b2d0de8482a4bd
Shmudoslave <149603728+Shmudoslave@users.noreply.github.com>
2024-06-28 00:03:46 +0300

    Rev.11
    Co-Authored-By: Alexmazhara <58631876+Alexmazhara@users.noreply.github.com>

--- Assets/scripts/CameraController1.cs
+++ Assets/scripts/CameraController1.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
@@ -10,6 +12,19 @@ public class CameraController : MonoBehaviour
 
     private Transform player;
     private int lastX;
+
+    [SerializeField]
+    float leftLimit;
+    [SerializeField]
+    float rightLimit;
+    [SerializeField]
+    float bottomLimit;
+    [SerializeField]
+    float upperLimit;
+
+
+
+
     
     
     void Start()
@@ -56,6 +71,22 @@ public class CameraController : MonoBehaviour
             transform.position = currentPosition;
         }
 
+        transform.position = new Vector3
+            (
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, upperLimit),
+            transform.position.z
+            );
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector2(leftLimit, upperLimit), new Vector2(rightLimit, upperLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(rightLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, upperLimit), new Vector2(leftLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, upperLimit), new Vector2(rightLimit, bottomLimit));
     }
 
 
